d0225729761610ff6958ca91b816c38b5185f540
Thomas <fitzgerald.thomas.e@gmail.com>
2022-12-28 21:57:47 -0500

    numbers and strings

--- studio_game.rb
+++ studio_game.rb
@@ -1,5 +1,17 @@
-greeting = "Welcome!"
-3.times do
-    puts greeting.upcase
-end
-puts Time.now
+# player1 = "larry"
+# health1 = 60
+
+# puts "#{player1}'s health is #{health1}"
+
+# puts "Players:\n\tlarry\n\tcurly\n\tmoe"
+
+player1 = "larry"
+health1 = 60
+
+player2 = "curly"
+health2 = 60
+
+player3 = "moe"
+health3 = 60
+
+puts "Players:\n\t#{player1}\n\t#{player2}\n\t#{player3}"
