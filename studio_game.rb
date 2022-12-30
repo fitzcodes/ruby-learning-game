6cc0bb46cf05c8368e9b3843ede3698e33ed814c
Thomas <fitzgerald.thomas.e@gmail.com>
2022-12-29 19:57:44 -0500

    variables and objects

--- studio_game.rb
+++ studio_game.rb
@@ -1,17 +1,17 @@
-# player1 = "larry"
-# health1 = 60
-
-# puts "#{player1}'s health is #{health1}"
-
-# puts "Players:\n\tlarry\n\tcurly\n\tmoe"
 
 player1 = "larry"
 health1 = 60
 
 player2 = "curly"
-health2 = 60
+health2 = 125
 
 player3 = "moe"
-health3 = 60
+health3 = 100
 
-puts "Players:\n\t#{player1}\n\t#{player2}\n\t#{player3}"
+player4 = "shemp"
+health4 = 90
+
+puts "#{player1.capitalize} has a health of #{health1}."
+puts "#{player2.upcase} has a health of #{health2}."
+puts "#{player3.capitalize} has a health of #{health3}.".center(50, '*')
+puts "#{player4.capitalize.ljust(30,'.')} #{health4}"
